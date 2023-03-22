fb9f9d1b0691cb8bf38b741f4c036a68af48cce1
FernandoCanabarro <127997952+FernandoCanabarro@users.noreply.github.com>
2023-03-22 18:51:57 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+a=float(input())               #int() pro computador entender que é um número inteiro
+c=input()
+b=float(input())               #float() pro computador entender que é um número decimal
+
+if c=="*":
+    print(a*b)
+
+elif c=="/":
+    print(a/b)
+
+elif c=="+":
+    print(a+b)
+
+elif c=="-":
+    print(a-b)
